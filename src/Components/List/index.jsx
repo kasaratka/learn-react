7d485870a03129834365e7f72a4492a04f4844e7
Jack Momot <jackmomot@gmail.com>
2022-01-31 19:28:23 +0200

    fix after review

---
+++ src/Components/List/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function List() {
+  const [comments, setComment] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInput = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addComment = () => {
+    if (!inputValue.length) return;
+    setComment([...comments, inputValue]);
+    setInputValue('');
+  };
+
+  return (
+    <div>
+      <label htmlFor="list">
+        Your comment:
+        <input id="list" value={inputValue} onInput={handleInput} />
+      </label>
+      <input type="submit" value="Submit" onClick={addComment} />
+      <div>
+        <ul>
+          {comments.map((item) => <li key={item.toString()}>{item}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default List;
